0a53f4bca604a151aa48671404e8ebf61fc30da6
Jairo Rojas <jairo142014@hotmail.com>
2020-08-08 22:51:03 -0500

    Componente principal terminado, conexion a DB Ready

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -13,43 +13,48 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      tasks: [
-        
-      ]
+      tasks: [],
     };
-    
     this.db = app.database().ref().child("tasks");
     this.addTask = this.addTask.bind(this);
+    this.removeTask = this.removeTask.bind(this);
   }
 
   componentDidMount() {
     const { tasks } = this.state;
-    this.db.on("child_added",snap => {
+    this.db.on("child_added", (snap) => {
       tasks.push({
         taskID: snap.key,
         taskTitle: snap.val().taskTitle,
         taskAuthor: snap.val().taskAuthor,
         taskDescription: snap.val().taskDescription,
         taskEstimatedTime: snap.val().taskEstimatedTime,
-        taskPriority: snap.val().taskPriority
+        taskPriority: snap.val().taskPriority,
       });
-      this.setState({tasks});
+      this.setState({ tasks });
+    });
+    this.db.on("child_removed", (snap) => {
+      for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].taskID == snap.key) {
+          tasks.splice(i, 1);
+        }
+      }
+      this.setState({ tasks });
     });
   }
 
-  removeTask() {}
+  removeTask(TaskID) {
+    this.db.child(TaskID).remove();
+  }
 
   addTask(item1, item2, item3, item4, item5) {
-    let { tasks } = this.state;
-    tasks.push({
-      taskID: tasks.length + 1,
+    this.db.push().set({
       taskTitle: item1,
       taskAuthor: item2,
       taskDescription: item4,
       taskEstimatedTime: item3,
       taskPriority: item5,
     });
-    this.setState({ tasks });
   }
 
   render() {
@@ -63,20 +68,29 @@ class App extends Component {
           OntaskEstimatedTime={task.taskEstimatedTime}
           OntaskPriority={task.taskPriority}
           key={task.taskID}
+          OnremoveTask={this.removeTask}
         />
       );
     });
-
+    console.log(this.state.tasks.length);
     return (
       <div className="backg">
-        <Navigation count={this.state.tasks.length} />
+        <Navigation />
         <div className="container mt-5 mb-5">
           <div className="row">
             <div className="col-lg-5">
               <FormTask addTask={this.addTask} />
             </div>
             <div className="col-lg-7">
-              <div className="row">{listTask}</div>
+              <div className="col-12 text-center lead mb-1">
+                <button type="button" class="btn btn-success mb-0 w-100">
+                  Tasks{" "}
+                  <span class="badge badge-light">
+                    {this.state.tasks.length}
+                  </span>
+                </button>
+              </div>
+              <div className="row scrollers rounded">{listTask}</div>
             </div>
           </div>
         </div>
